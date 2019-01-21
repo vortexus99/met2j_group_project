c74c4b6be5527820c5a75d2532af9a6c6658b8ab
Jana Elbrecht <j.s.elbrecht@students.uu.nl>
2019-01-21 16:41:35 +0100

    Occupations now as list

--- main_function.py
+++ main_function.py
@@ -24,17 +24,25 @@ for indx, entry in enumerate(test):
 
 print(counter)'''
 
-occupations = ["actor", "politician", "athlete", "musician"]
+occupations = ["actor", "politician", "athlete", "musician", "sing", "songwriter", "prince"]
+
 
 for entry in json_filtered:
+    Occupation_g = []
+    entry["Occupation_g"]=Occupation_g
     for occupation in occupations:
         if "occupation_label" in entry:
-            if occupation in entry["occupation_label"].lower():
-                entry["Occupation_g"]=occupation 
+            if occupation not in Occupation_g:
+                if occupation in entry["occupation_label"].lower():
+                    Occupation_g.append(occupation) 
         if "occupation" in entry:
             if occupation in entry["occupation"].lower():
-                entry["Occupation_g"]=occupation 
-
+                if occupation not in Occupation_g:
+                    Occupation_g.append(occupation) 
+        if "title" in entry:
+            if occupation not in Occupation_g:
+                if occupation in entry["title"].lower():
+                    Occupation_g.append(occupation) 
 
 with open("json_test.json",'w') as file:
     json.dump(json_filtered, file, indent=4)

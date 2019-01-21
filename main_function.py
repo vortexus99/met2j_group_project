8417cb706e4cf4aa20e49b793cfc463b2c264608
Jana Elbrecht <j.s.elbrecht@students.uu.nl>
2019-01-21 16:08:10 +0100

    Search function for occupations

--- main_function.py
+++ main_function.py
@@ -24,6 +24,17 @@ for indx, entry in enumerate(test):
 
 print(counter)'''
 
+occupations = ["actor", "politician", "athlete", "musician"]
+
+for entry in json_filtered:
+    for occupation in occupations:
+        if "occupation_label" in entry:
+            if occupation in entry["occupation_label"].lower():
+                entry["Occupation_g"]=occupation 
+        if "occupation" in entry:
+            if occupation in entry["occupation"].lower():
+                entry["Occupation_g"]=occupation 
+
 
 with open("json_test.json",'w') as file:
     json.dump(json_filtered, file, indent=4)
